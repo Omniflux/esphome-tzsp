1c05276e63c1d459bdbf515ff3f3ce11e07b7560
Omni Flux <omniflux@omniflux.com>
2023-11-27 14:29:07 -0700

    Initialize IP stack if not already initialized

--- components/tzsp/tzsp.h
+++ components/tzsp/tzsp.h
@@ -24,7 +24,10 @@ constexpr u_int8_t TZSP_HEADER_LENGTH = 5;
 
 class TZSPSender {
   public:
-    TZSPSender() = default;
+    TZSPSender() {
+      ESP_ERROR_CHECK(esp_netif_init());
+      this->tzsp_socket_ = socket::socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+    }
 
     void set_tzsp_ip(const network::IPAddress ip) { this->tzsp_sockaddr_in_.sin_addr.s_addr = static_cast<ip_addr_t>(ip).addr; }
     void set_tzsp_port(uint16_t port) { this->tzsp_sockaddr_in_.sin_port = htons(port); }
@@ -34,14 +37,9 @@ class TZSPSender {
     void tzsp_send(const T& container) {
       if (this->tzsp_sockaddr_in_.sin_addr.s_addr != ESPHOME_INADDR_ANY)
       {
-        if (this->tzsp_socket_ == nullptr)
-          this->tzsp_socket_ = socket::socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-
-        if (this->tzsp_socket_ != nullptr) {
-          std::vector<uint8_t> buffer { 1, 1, static_cast<uint8_t>(this->tzsp_protocol_), static_cast<uint8_t>(this->tzsp_protocol_ >> 8), 1 };
-          buffer.insert(buffer.end(), std::begin(container), std::end(container));
-          this->tzsp_socket_->sendto(buffer.data(), buffer.size(), 0, reinterpret_cast<sockaddr*>(&this->tzsp_sockaddr_in_), sizeof(this->tzsp_sockaddr_in_));
-        }
+        std::vector<uint8_t> buffer { 1, 1, static_cast<uint8_t>(this->tzsp_protocol_), static_cast<uint8_t>(this->tzsp_protocol_ >> 8), 1 };
+        buffer.insert(buffer.end(), std::begin(container), std::end(container));
+        this->tzsp_socket_->sendto(buffer.data(), buffer.size(), 0, reinterpret_cast<sockaddr*>(&this->tzsp_sockaddr_in_), sizeof(this->tzsp_sockaddr_in_));
       }
     }
 
