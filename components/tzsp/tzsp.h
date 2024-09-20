8d8cc2cb9d33fe00470483e6049493dc7384c156
Omni Flux <omniflux@omniflux.com>
2024-09-19 18:50:36 -0600

    Wait for IP stack to be initialized, instead of initializing here

--- components/tzsp/tzsp.h
+++ components/tzsp/tzsp.h
@@ -24,19 +24,17 @@ constexpr u_int8_t TZSP_HEADER_LENGTH = 5;
 
 class TZSPSender {
   public:
-    TZSPSender() {
-      ESP_ERROR_CHECK(esp_netif_init());
-      this->tzsp_socket_ = socket::socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
-    }
-
-    void set_tzsp_ip(const network::IPAddress ip) { this->tzsp_sockaddr_in_.sin_addr.s_addr = static_cast<ip_addr_t>(ip).addr; }
+    void set_tzsp_ip(const network::IPAddress& ip) { this->tzsp_sockaddr_in_.sin_addr.s_addr = static_cast<ip_addr_t>(ip).addr; }
     void set_tzsp_port(uint16_t port) { this->tzsp_sockaddr_in_.sin_port = htons(port); }
     void set_tzsp_protocol(uint16_t protocol) { this->tzsp_protocol_ = htons(protocol); }
 
     template<class T>
     void tzsp_send(const T& container) {
-      if (this->tzsp_sockaddr_in_.sin_addr.s_addr != ESPHOME_INADDR_ANY)
+      if (this->tzsp_sockaddr_in_.sin_addr.s_addr != ESPHOME_INADDR_ANY && sys_thread_tcpip(LWIP_CORE_IS_TCPIP_INITIALIZED))
       {
+        if (!this->tzsp_socket_)
+          this->tzsp_socket_ = socket::socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
+
         std::vector<uint8_t> buffer { 1, 1, static_cast<uint8_t>(this->tzsp_protocol_), static_cast<uint8_t>(this->tzsp_protocol_ >> 8), 1 };
         buffer.insert(buffer.end(), std::begin(container), std::end(container));
         this->tzsp_socket_->sendto(buffer.data(), buffer.size(), 0, reinterpret_cast<sockaddr*>(&this->tzsp_sockaddr_in_), sizeof(this->tzsp_sockaddr_in_));
@@ -45,7 +43,7 @@ class TZSPSender {
 
   protected:
     uint16_t tzsp_protocol_{};
-    std::unique_ptr<socket::Socket> tzsp_socket_;
+    std::unique_ptr<socket::Socket> tzsp_socket_{};
     struct sockaddr_in tzsp_sockaddr_in_ { sizeof(struct sockaddr_in), AF_INET, htons(TZSP_PORT), { ESPHOME_INADDR_ANY }, 0 };
     static_assert(offsetof(sockaddr_in, sin_family) != 0, "sin_size removed from sockaddr_in - fix initializer");
 };
